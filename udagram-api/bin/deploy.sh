54adc51f0a21bfbea94830e05438da460bc3e898
0mar-aly <94472899+0mar-aly@users.noreply.github.com>
2022-04-15 03:32:31 +0200

    changing s3 bucket and eb environment

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
 eb init udagram-api --platform node.js --region $AWS_DEFAULT_REGION &&
-eb use udagram-api-dev &&
+eb use udagram-api-prod &&
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT &&
 eb setenv PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE &&
 eb setenv AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET &&
-eb deploy udagram-api-dev
+eb deploy udagram-api-prod
